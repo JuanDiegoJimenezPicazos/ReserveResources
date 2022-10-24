12a01176fbdc334808e161c26067a0679a796848
Juan Diego <juan@email.com>
2022-10-24 10:40:33 +0200

    Tabla users funcionando

---
+++ views/users/all.php
@@ -0,0 +1,40 @@
+<?php
+// VISTA PARA LA LISTA DE LIBROS
+
+// Recuperamos la lista de libros
+$usersList = $data["showList"];
+
+// Si hay algún mensaje de feedback, lo mostramos
+// if (isset($data["info"])) {
+//   echo "<div style='color:blue'>".$data["info"]."</div>";
+// }
+
+// if (isset($data["error"])) {
+//   echo "<div style='color:red'>".$data["error"]."</div>";
+// }
+
+// echo "<form action='index.php'>
+//         <input type='hidden' name='action' value='buscarLibros'>
+//         <input type='text' name='textoBusqueda'>
+//         <input type='submit' value='Buscar'>
+//       </form><br>";
+
+// Ahora, la tabla con los datos de los libros
+echo "<h2>Usuarios</h2>";
+if (count($usersList) == 0) {
+  echo "No hay datos";
+} else {
+  echo "<table>";
+  foreach ($usersList as $fila) {
+    echo "<tr>";
+    echo "<td>" . $fila->username . "</td>";
+    echo "<td>" . $fila->password . "</td>";
+    echo "<td>" . $fila->realname . "</td>";
+    echo "<td>" . $fila->type . "</td>";
+    echo "<td><a href='index.php?controller=usersController&action=modifyUsersForm&id=" . $fila->id . "'>Modificar</a></td>";
+    echo "<td><a href='index.php?controller=usersController&action=eraseUsers&id=" . $fila->id . "'>Borrar</a></td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+}
+echo "<p><a href='index.php?controller=usersController&action=insertUsersForm'>Nuevo tramo horario</a></p>";
---
+++ views/users/form.php
@@ -0,0 +1,60 @@
+<?php
+// VISTA PARA INSERCIÓN/EDICIÓN DE LIBROS
+if ($data) {
+    extract($data);   // Extrae el contenido de $data y lo convierte en variables individuales ($libro, $todosLosAutores y $autoresLibro)
+}
+
+// Vamos a usar la misma vista para insertar y modificar. Para saber si hacemos una cosa u otra,
+// usaremos la variable $libro: si existe, es porque estamos modificando un libro. Si no, estamos insertando uno nuevo.
+if (isset($users)) {   
+    echo "<h1>Modificación de recursos</h1>";
+} else {
+    echo "<h1>Inserción de recursos</h1>";
+}
+
+// Sacamos los datos del libro (si existe) a variables individuales para mostrarlo en los inputs del formulario.
+// (Si no hay libro, dejamos los campos en blanco y el formulario servirá para inserción).
+$id = $users->id ?? ""; 
+$username = $users->username ?? "";
+$password = $users->password ?? "";
+$realname = $users->realname ?? "";
+$type = $users->type ?? "";
+
+// Creamos el formulario con los campos del libro
+echo "<form action = 'index.php' method = 'get'>
+        <input type='hidden' name='id' value='".$id."'>
+        Nombre de usuario:<input type='text' name='username' value='".$username."'><br>
+        Contraseña:<input type='text' name='password' value='".$password."'><br>
+        Nombre real:<input type='text' name='realname' value='".$realname."'><br>
+        Tipo admin o user:<input type='text' name='type' value='".$type."'><br>";
+
+
+        // <label for="days">Día de la semana:</label>
+        // <select name="dayOfWeek" value='".$dayOfWeek."'>
+        //   <option value="lunes">Lunes</option>
+        //   <option value="martes">Martes</option>
+        //   <option value="miercoles">Miercoles</option>
+        //   <option value="jueves">Jueves</option>
+        //   <option value="viernes">Viernes</option>
+        // </select>
+
+
+// echo "Autores: <select name='autor[]' multiple size='3'>";
+// foreach ($todosLosAutores as $fila) {
+//     if (in_array($fila->idPersona, $autoresLibro))
+//         echo "<option value='$fila->idPersona' selected>$fila->nombre $fila->apellido</option>";
+//     else
+//         echo "<option value='$fila->idPersona'>$fila->nombre $fila->apellido</option>";
+// }
+//echo "</select>";
+
+echo "<input type='hidden' name='controller' value='usersController'>";
+
+// Finalizamos el formulario
+if (isset($users)) {
+    echo "  <input type='hidden' name='action' value='modifyUsers'>";
+} else {
+    echo "  <input type='hidden' name='action' value='insertUsers'>";
+}
+echo "	<input type='submit'></form>";
+echo "<p><a href='index.php'>Volver</a></p>";
